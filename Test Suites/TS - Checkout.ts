51a03bd894b56c39e63f2cd40eb91a10d85563f8
Rajasa Cikal <79238217+cikalmaulana@users.noreply.github.com>
2024-02-13 10:34:43 +0700

    Add Test Case Search and Checkout, Add Objects

---
+++ Test Suites/TS - Checkout.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS - Checkout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>33988769-baba-4179-8aa1-635592c58874</testSuiteGuid>
+</TestSuiteEntity>
